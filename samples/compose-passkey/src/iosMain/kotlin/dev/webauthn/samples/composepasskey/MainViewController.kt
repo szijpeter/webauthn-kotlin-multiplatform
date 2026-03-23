3410081fc9777182a5f86e1bc89a8451961d38ef
Peter Szij <szijpeter@gmail.com>
2026-03-23 23:18:07 +0100

    samples: harden iOS plist sanity for compose host app

--- samples/compose-passkey/src/iosMain/kotlin/dev/webauthn/samples/composepasskey/MainViewController.kt
+++ samples/compose-passkey/src/iosMain/kotlin/dev/webauthn/samples/composepasskey/MainViewController.kt
@@ -2,4 +2,9 @@ package dev.webauthn.samples.composepasskey
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-public fun MainViewController() = ComposeUIViewController { App() }
+public fun MainViewController() = ComposeUIViewController(
+    configure = {
+        // Keep the sample app runnable even when host apps use generated plist settings.
+        enforceStrictPlistSanityCheck = false
+    },
+) { App() }
